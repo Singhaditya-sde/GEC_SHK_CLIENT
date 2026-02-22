0c14f99b644e9d07ca1d902094940106969c6473
Aditya Kumar <singhaditya.cs@gmail.com>
2026-02-23 02:13:42 +0530

    Adding the parent routes for the student and faculty

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import './index.css'
-import { AdminDashboard } from './modules/Dashboard/Pages/AdminDashboard'
+import { Routes, Route ,Navigate } from 'react-router'
+import { AdminLayout } from './Layout/AminLayout'
+import { AdminDashboard } from './modules/admin/admin_pages/Admin_Dashboard'
+import { AdminStudentPage } from './modules/admin/admin_pages/Admin_Student_Page'
+import { AdminBulkUploadPage } from './modules/admin/admin_pages/Admin_Bulk_UploadPage'
+import { AdminFacultyPage } from './modules/admin/admin_pages/Admin_Faculty_Page'
+import { AdminDepartmentPage } from './modules/admin/admin_pages/Admin_Department_Page'
+import { StudentDirectory } from './components/Students/StudentDirectory'
+import { AdminAddFacultyPage } from './modules/admin/admin_pages/Admin_Add_FacultyPage'
+
+
 
 function App() {
   return (
     <>
-      <div className="">
-        <AdminDashboard />
-      </div>
+      <Routes>
+
+      <Route path="/" element={<Navigate to="/admin" replace />} />
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route index element={<AdminDashboard />} />
+        <Route path="dashboard" element={<AdminDashboard />} />
+        {/* Students Parent Route */}
+        <Route path="students">
+          <Route index element={<AdminStudentPage />} />
+          <Route path="bulk-upload" element={<AdminBulkUploadPage />} />
+        </Route>
+        <Route path='faculty'>
+          <Route index element={<AdminFacultyPage />}/>
+          <Route path='add_faculty' element={<AdminAddFacultyPage />}/>
+        </Route>
+        <Route path="departments" element={<AdminDepartmentPage />} />
+        <Route path="directory" element={<StudentDirectory />} />
+      </Route>
+    </Routes>
     </>
   )
 }
