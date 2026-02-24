daa5bfb7cf7e2ad15adff7df46dc7c6ff71b48d9
Aditya Kumar <singhaditya.cs@gmail.com>
2026-02-25 01:18:11 +0530

    Add store dir

---
+++ src/store/authStore.ts
@@ -0,0 +1,30 @@
+import { create } from "zustand";
+
+interface User {
+  id: string;
+  role: string;
+}
+
+interface AuthState {
+  user: User | null;
+  isAuthenticated: boolean;
+  login: (user: User) => void;
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  isAuthenticated: false,
+
+  login: (user) =>
+    set({
+      user,
+      isAuthenticated: true,
+    }),
+
+  logout: () =>
+    set({
+      user: null,
+      isAuthenticated: false,
+    }),
+}));
