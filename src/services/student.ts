cef37b631ef13c7f6250e9903f98a48b94bc23cc
Aditya Kumar <singhaditya.cs@gmail.com>
2026-03-09 22:03:24 +0530

    Add services directory for the api calling

--- src/services/student.ts
+++ src/services/student.ts
@@ -1,24 +1,3 @@
-// import api from "./api"
-// import type { StudentFormData } from "@/types/student"
-
-// export const registerSingleStudent = async (
-//   data: StudentFormData
-// ) => {
-//   const token = localStorage.getItem("token")
-
-//   const response = await api.post(
-//     "/api/auth/register/student",
-//     data,
-//     {
-//       headers: {
-//         Authorization: `Bearer ${token}`,
-//       },
-//     }
-//   )
-
-//   return response.data
-// }
-
 import api from "./api"
 import type { StudentRegisterInput } from "@/types/student"
 
