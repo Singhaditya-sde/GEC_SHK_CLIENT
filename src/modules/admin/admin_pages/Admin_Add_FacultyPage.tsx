79957dc7af5592de1009620f2480504bd2076998
Aditya Kumar <singhaditya.cs@gmail.com>
2026-03-12 01:37:27 +0530

    Fixing the ui and connect with the backend

--- src/modules/admin/admin_pages/Admin_Add_FacultyPage.tsx
+++ src/modules/admin/admin_pages/Admin_Add_FacultyPage.tsx
@@ -1,17 +1,37 @@
 import { ToggleCard } from "@/components/ToggleCard";
 import { useState } from "react";
-import { ShieldCheck , CheckCircle } from "lucide-react";
+import { ShieldCheck , CheckCircle , AlertCircle } from "lucide-react";
 import type { FacultyForm } from "@/types/faculty";
+import { registerFaculty } from "@/services/faculty";
+import { Spinner } from "@/components/common/Spinner";
 
 export function AdminAddFacultyPage() {
 
 const [loading, setLoading] = useState(false)
 const [success, setSuccess] = useState(false)
 const [errors, setErrors] = useState<Partial<Record<keyof FacultyForm, string>>>({})
+const [serverError, setServerError] = useState("")
+const [formData, setFormData] = useState<FacultyForm>({
+name: "",
+email: "",
+phoneNo: "",
+regNo: "",
+password: "",
+deptId: 0,
+isHOD: false
+});
 
 //this is the function for the validate the form
 function validateForm(): boolean {
   const newErrors: Partial<Record<keyof FacultyForm, string>> = {}
+
+  if (!formData.email.trim()) {
+  newErrors.email = "Email is required"
+  }
+
+  if (!formData.password.trim()) {
+    newErrors.password = "Password is required"
+  }
 
   if (!formData.name.trim()) {
     newErrors.name = "Full Name is required"
@@ -35,39 +55,63 @@ function validateForm(): boolean {
 }
 
 // this is function is for the handle the form submit
-function handleSubmit(e: React.FormEvent) {
+async function handleSubmit(e: React.FormEvent) {
   e.preventDefault()
 
   if (!validateForm()) return
 
-  setLoading(true)
+  try {
+    setLoading(true)
+    setErrors({})
+    setServerError('')
 
-  setTimeout(() => {
-    setLoading(false)
+    await registerFaculty(formData)
+
     setSuccess(true)
 
-    // reset after showing success message
+    setFormData({
+      name: "",
+      email: "",
+      phoneNo: "",
+      password: "",
+      regNo: "",
+      deptId: 0,
+      isHOD: false
+    })
+
     setTimeout(() => {
       setSuccess(false)
-      setFormData({
-        name: "",
-        phoneNo: "",
-        regNo: "",
-        deptId: "",
-        isHOD: false
-      })
     }, 2000)
 
-  }, 1000)
+  } catch (err: any) {
+
+  const message =
+    err.response?.data?.message || "Internal server error"
+
+  if (message === "email already exists") {
+    setErrors(prev => ({
+      ...prev,
+      email: message
+    }))
+  }
+
+  else if (message === "faculty data already exists") {
+    setErrors(prev => ({
+      ...prev,
+      regNo: "Registration number already exists"
+    }))
+  }
+
+  else {
+    setServerError(message)
+  }
+
+}
+finally {
+    setLoading(false)
+  }
 }
 
-  const [formData, setFormData] = useState<FacultyForm>({
-  name: "",
-  phoneNo: "",
-  regNo: "",
-  deptId: 0,
-  isHOD: false
-  });
 
   return (
     <div className="px-5">
@@ -92,7 +136,9 @@ function handleSubmit(e: React.FormEvent) {
                 onChange={(e) =>
                   setFormData({ ...formData, name: e.target.value })
                 }
-                className="border bg-[#F8FAFC] rounded-xl px-3 py-2 focus:ring-2 focus:ring-indigo-500 outline-none"
+                className={`w-full border bg-[#F8FAFC] rounded-xl px-3 py-2 text-md 
+                  focus:ring-2 focus:ring-indigo-500 outline-none appearance-none
+                  ${errors.name ? 'border-red-500' : 'border-[#E2E8F0]'}`}
               />
               {errors.name && (
                 <p className="text-red-500 text-xs mt-1">{errors.name}</p>
@@ -100,21 +146,25 @@ function handleSubmit(e: React.FormEvent) {
             </div>
             <div className="flex flex-col gap-1.5">
               <label className="text-sm font-medium text-[#334155]">
-                Phone No <span className="text-red-400">*</span>
+                Faculty Email <span className="text-red-400">*</span>
               </label>
               <input
-                type="tel"
-                placeholder="Enter Phone No"
-                pattern="[0-9]{10}"
-                maxLength={10}
-                value={formData.phoneNo}
-                onChange={(e) =>
-                  setFormData({ ...formData, phoneNo: e.target.value })
-                }
-                className="border bg-[#F8FAFC] rounded-xl px-3 py-2 focus:ring-2 focus:ring-indigo-500 outline-none"
+                type="email"
+                placeholder="Enter the Email"
+                value={formData.email}
+                onChange={(e) => {
+                  setFormData(prev => ({
+                    ...prev,
+                    email: e.target.value
+                  }))
+                  setErrors(prev => ({ ...prev, email: "" }))
+                }}
+                className={`w-full border bg-[#F8FAFC] rounded-xl px-3 py-2 text-md 
+                  focus:ring-2 focus:ring-indigo-500 outline-none appearance-none
+                  ${errors.email ? 'border-red-500' : 'border-[#E2E8F0]'}`}
               />
-              {errors.phoneNo && (
-                <p className="text-red-500 text-xs mt-1">{errors.phoneNo}</p>
+              {errors.email && (
+                <p className="text-red-500 text-xs mt-1">{errors.email}</p>
               )}
             </div>
           </div>
@@ -128,10 +178,16 @@ function handleSubmit(e: React.FormEvent) {
                 type="text"
                 placeholder="Registration Number"
                 value={formData.regNo}
-                onChange={(e) =>
-                  setFormData({ ...formData, regNo: e.target.value })
-                }
-                className="border bg-[#F8FAFC] rounded-xl px-3 py-2 focus:ring-2 focus:ring-indigo-500 outline-none"
+                onChange={(e) => {
+                  setFormData(prev => ({
+                    ...prev,
+                    regNo: e.target.value
+                  }))
+                  setErrors(prev => ({ ...prev, regNo: "" }))
+                }}
+                className={`w-full border bg-[#F8FAFC] rounded-xl px-3 py-2 text-md 
+                  focus:ring-2 focus:ring-indigo-500 outline-none appearance-none
+                  ${errors.regNo ? 'border-red-500' : 'border-[#E2E8F0]'}`}
               />
               {errors.regNo && (
                 <p className="text-red-500 text-xs mt-1">{errors.regNo}</p>
@@ -146,9 +202,11 @@ function handleSubmit(e: React.FormEvent) {
                 onChange={(e) =>
                   setFormData({ ...formData, deptId: Number(e.target.value) })
                 }
-                className="border bg-[#F8FAFC] rounded-xl px-3 py-2 focus:ring-2 focus:ring-indigo-500 outline-none"
+                className={`w-full border bg-[#F8FAFC] rounded-xl px-3 py-2 text-md 
+                  focus:ring-2 focus:ring-indigo-500 outline-none appearance-none
+                  ${errors.deptId ? 'border-red-500' : 'border-[#E2E8F0]'}`}
               >
-                <option value="">Select Department</option>
+                <option value={0}>Select Department</option>
                 <option value={1}>CSE</option>
                 <option value={2}>EE</option>
                 <option value={3}>ECE</option>
@@ -158,6 +216,50 @@ function handleSubmit(e: React.FormEvent) {
                 <p className="text-red-500 text-xs mt-1">{errors.deptId}</p>
               )}
             </div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-10 mt-5">
+            <div className="flex flex-col gap-1.5">
+              <label className="text-sm font-medium text-[#334155]">
+                Phone No <span className="text-red-400">*</span>
+              </label>
+              <input
+                type="tel"
+                placeholder="Enter Phone No"
+                pattern="[0-9]{10}"
+                maxLength={10}
+                value={formData.phoneNo}
+                onChange={(e) =>
+                  setFormData({ ...formData, phoneNo: e.target.value })
+                }
+                className={`w-full border bg-[#F8FAFC] rounded-xl px-3 py-2 text-md 
+                  focus:ring-2 focus:ring-indigo-500 outline-none appearance-none
+                  ${errors.phoneNo ? 'border-red-500' : 'border-[#E2E8F0]'}`}
+              />
+              {errors.phoneNo && (
+                <p className="text-red-500 text-xs mt-1">{errors.phoneNo}</p>
+              )}
+            </div>
+            <div className="flex flex-col gap-1.5">
+              <label className="text-sm font-medium text-[#334155]">
+                Password <span className="text-red-400">*</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Password Min of 8"
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
+                className={`w-full border bg-[#F8FAFC] rounded-xl px-3 py-2 text-md 
+                  focus:ring-2 focus:ring-indigo-500 outline-none appearance-none
+                  ${errors.password ? 'border-red-500' : 'border-[#E2E8F0]'}`}
+              />
+              {errors.password && (
+                <p className="text-red-500 text-xs mt-1">{errors.password}</p>
+              )}
+            </div>
+            
           </div>
 
           <div className="grid grid-cols gap-10 mt-5">
@@ -179,27 +281,41 @@ function handleSubmit(e: React.FormEvent) {
             </div>
           </div>
 
-          <div className="space-y-2 mt-5 flex justify-end">
-              <button
-                type="submit"
-                disabled={loading}
-                className={`
-                  px-10 py-3 text-white text-sm rounded-2xl flex items-center gap-3 
-                  transition-all duration-300 ease-in-out cursor-pointer
-                  ${success ? "bg-green-600" : "bg-[#0B3D93]"}
-                  ${loading ? "opacity-70 cursor-not-allowed" : ""}
-                `}
-              >
-                {loading ? ("Submitting...") : success ? 
-                (
-                  <> 
-                    <CheckCircle size={20} className="animate-scaleIn" /> Registered Successfully
-                  </>
-                ) : (
-                  "Register Faculty"
-                )}
-              </button>
-            </div>
+          <div className="mt-5 flex justify-end items-center gap-4">
+
+            {serverError && (
+              <div className="h-[44px] px-6 bg-red-100 text-red-700 text-sm rounded-2xl flex items-center gap-2">
+                <AlertCircle size={16} />
+                {serverError}
+              </div>
+            )}
+
+            <button
+              type="submit"
+              disabled={loading}
+              className={`
+                h-[44px] px-10 text-white text-sm rounded-2xl flex items-center gap-3
+                transition-all duration-300 ease-in-out cursor-pointer
+                ${success ? "bg-green-600" : "bg-[#0B3D93]"}
+                ${loading ? "opacity-70 cursor-not-allowed" : ""}
+              `}
+            >
+              {loading ? (
+                <>
+                <Spinner />
+                  Submitting...
+                </>
+              ) : success ? (
+                <>
+                  <CheckCircle size={20} className="animate-scaleIn" />
+                  Registered Successfully
+                </>
+              ) : (
+                "Register Faculty"
+              )}
+            </button>
+
+          </div>
         </form>
       </div> 
   </div>
