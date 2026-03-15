38f044ecac9897bca6a99efefc10741ff5e60436
Aditya Kumar <singhaditya.cs@gmail.com>
2026-03-15 19:05:04 +0530

    Add api for the student

--- src/services/student.ts
+++ src/services/student.ts
@@ -1,13 +1,8 @@
-import api from "./api"
-import type { StudentRegisterInput } from "@/types/student"
+import api from './api'
+import type { StudentRegisterInput } from '@/types/student'
 
-export const registerSingleStudent = async (
-  data: StudentRegisterInput
-) => {
-  const response = await api.post(
-    "/api/auth/register/student",
-    data
-  )
+export const registerSingleStudent = async (data: StudentRegisterInput) => {
+  const response = await api.post('/api/auth/register/student', data)
 
   return response.data
 }
