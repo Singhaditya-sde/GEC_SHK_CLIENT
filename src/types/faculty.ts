ed8f8b447e94f59f1cd2d810f0bb2bc27c205adf
Aditya Kumar <singhaditya.cs@gmail.com>
2026-03-15 18:53:52 +0530

    Add data for the faculty

--- src/types/faculty.ts
+++ src/types/faculty.ts
@@ -1,9 +1,23 @@
 export interface FacultyForm {
-  name: string;
-  email: string;
-  phoneNo: string;
+  name: string
+  email: string
+  phoneNo: string
   regNo: string
-  deptId: number | ""
-  password: string;
+  deptId: number | ''
+  password: string
   isHOD: boolean
+}
+
+export interface Faculty {
+  id: number
+  name: string
+  designation: string
+  contact: number
+  email: string
+  department: string
+  publications: number
+  experience: number
+  projects: number
+  skills: string
+  summary: string
 }
