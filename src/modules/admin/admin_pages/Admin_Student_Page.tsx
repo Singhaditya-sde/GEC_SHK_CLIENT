cb2a6304e937fb3ca6ff00fef3f927f08d83ef24
Aditya Kumar <singhaditya.cs@gmail.com>
2026-03-16 23:41:19 +0530

    update admin student page

--- src/modules/admin/admin_pages/Admin_Student_Page.tsx
+++ src/modules/admin/admin_pages/Admin_Student_Page.tsx
@@ -2,43 +2,85 @@ import { GraduationCap, Users, Building2, Plus } from 'lucide-react'
 import { StudentDirectory } from './StudentDirectory'
 import { BatchCard } from '../admin_components/BatchCard'
 import { Link } from 'react-router'
+import { useState , useEffect } from 'react'
+import { getStudentsForAdmin } from '@/services/studentApi'
+import { Spinner } from '@/components/common/Spinner'
 
-const StudentdashboardStats = [
+type StudentAdminStats = {
+  allStudents: {
+    students: any[]
+    pagination: {
+      total: number
+      page: number
+      limit: number
+      totalPages: number
+    }
+  }
+  newlyAdmittedStudent: number
+  totalActiveStudents: number
+  
+}
+
+export default function AdminStudentPage() {
+const [stats, setStats] = useState<StudentAdminStats | null>(null)
+
+useEffect(() => {
+  const fetchedStats = async () => {
+    try {
+      const data = await getStudentsForAdmin()
+      setStats(data)
+    } catch (error) {
+      console.error("Failed to fetch student stats", error)
+    }
+  }
+  fetchedStats()
+}, [])
+  
+if(!stats) return (
+  <div className='flex justify-center items-center h-full'>
+    <Spinner size={28}/>
+  </div>
+)
+
+const totalStudents = stats.allStudents.pagination.total
+const activeStudents = stats.totalActiveStudents
+const newAdmissions = stats.newlyAdmittedStudent
+
+const studentDashboardStats = [
   {
-    id: 'students',
+    id: "totalStudents",
     icon: GraduationCap,
-    iconColor: '#0B3D93',
-    iconBgColor: 'rgba(11, 61, 147, 0.1)',
-    title: 'Total Students',
-    value: '2,204',
+    iconColor: "#0B3D93",
+    iconBgColor: "rgba(11, 61, 147, 0.1)",
+    title: "Total Students",
+    value: totalStudents,
   },
   {
-    id: 'students',
+    id: "activeStudents",
     icon: Users,
-    iconColor: '#4F46E5',
-    iconBgColor: 'rgba(79, 70, 229, 0.1)',
-    title: 'Active Students',
-    value: 40,
+    iconColor: "#4F46E5",
+    iconBgColor: "rgba(79, 70, 229, 0.1)",
+    title: "Active Students",
+    value: activeStudents,
   },
   {
-    id: 'students',
+    id: "departments",
     icon: Building2,
-    iconColor: '#0D9488',
-    iconBgColor: 'rgba(13, 148, 136, 0.1)',
-    title: 'Departments',
+    iconColor: "#0D9488",
+    iconBgColor: "rgba(13, 148, 136, 0.1)",
+    title: "Departments",
     value: 5,
   },
   {
-    id: 'students',
+    id: "newAdmissions",
     icon: GraduationCap,
-    iconColor: '#DC2626',
-    iconBgColor: 'rgba(220, 38, 38, 0.1)',
-    title: 'New Adimission',
-    value: 48,
+    iconColor: "#DC2626",
+    iconBgColor: "rgba(220, 38, 38, 0.1)",
+    title: "New Admission",
+    value: newAdmissions,
   },
 ]
 
-export default function AdminStudentPage() {
   return (
     <div className="">
       <div className="flex justify-between items-center p-5">
@@ -58,16 +100,19 @@ export default function AdminStudentPage() {
         </div>
       </div>
       <div className="flex gap-5 px-5">
-        {StudentdashboardStats.map(({ id, icon, iconColor, iconBgColor, title, value }) => (
-          <BatchCard
-            key={id}
-            icon={icon}
-            iconColor={iconColor}
-            iconBgColor={iconBgColor}
-            title={title}
-            value={value}
-          />
-        ))}
+        {studentDashboardStats.map(
+          ({ id, icon, iconColor, iconBgColor, title, value }) => (
+            <BatchCard
+              key={id}
+              icon={icon}
+              iconColor={iconColor}
+              iconBgColor={iconBgColor}
+              title={title}
+              value={value}
+              className="w-full lg:py-13"
+            />
+          )
+        )}
       </div>
       <div className="pb-5">
         <StudentDirectory />
