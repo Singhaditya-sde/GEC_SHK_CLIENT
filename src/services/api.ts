0d5e6dab8baa7d881c5326337bf59832cd0d2eae
Aditya Kumar <singhaditya.cs@gmail.com>
2026-03-17 01:57:15 +0530

    refactor: update login page and api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,19 @@
-// import axios from 'axios'
-
-// const api = axios.create({
-//   baseURL: 'http://localhost:8000',
-//   withCredentials: true,
-// })
-
-// export default api
-
-
-const API_URL = import.meta.env.VITE_API_URL;
-import axios from "axios";
+import axios from 'axios'
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: 'http://localhost:8000',
   withCredentials: true,
-});
+})
 
-export default api;
+export default api
+
+
+// const API_URL = import.meta.env.VITE_API_URL;
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: API_URL,
+//   withCredentials: true,
+// });
+
+// export default api;
